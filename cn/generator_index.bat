642c88ccadcc7e6f3c75bda4b95ae34b3c64130d
ruanjf <eeqqbb@gmail.com>
2013-01-01 16:34:43 +0800

    修改文件路径

--- cn/generator_index.bat
+++ cn/generator_index.bat
@@ -1,6 +1,6 @@
-cd node-doc-generator
+cd ..\node-doc-generator
 
-node .\generate.js --format=html --template=../source/template.html --out=../v0.8.16 ../source/v0.8.16/index.markdown
+node .\generate.js --format=html --template=../cn/template.html --out=../v0.8.16 ../cn/v0.8.16/index.markdown
 
 git commit -am "update doc"
 
